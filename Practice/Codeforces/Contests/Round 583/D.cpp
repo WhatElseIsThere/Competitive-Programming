65e885ee6989caf69e2b992315a0926115bd86ba
Artem Plotkin <arxmath@gmail.com>
2019-09-06 01:22:20 +0300

    Add problems B, C, D from CF Round 583

---
+++ Practice/Codeforces/Contests/Round 583/D.cpp
@@ -0,0 +1,68 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+using ll = long long;
+using ld = long double;
+
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+
+#ifdef LOCAL
+    #define eprint(x) cerr << #x << " = " << (x) << endl
+    #define eprintf(args...) fprintf(stderr, args), fflush(stderr)
+#else
+    #define eprint(x)
+    #define eprintf(...)
+#endif
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m;
+    cin >> n >> m;
+
+    vector<vector<char>> a(n, vector<char>(m));
+    for (int i = 0; i < n; i++)
+        for (int j = 0; j < m; j++)
+            cin >> a[i][j];
+
+    vector<vector<bool>> b1(n, vector<bool>(m));
+    b1[0][0] = true;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            if (a[i][j] == '#')
+                continue;
+            if (i > 0 && b1[i - 1][j])
+                b1[i][j] = true;
+            if (j > 0 && b1[i][j - 1])
+                b1[i][j] = true;
+        }
+    }
+
+    vector<vector<bool>> b2(n, vector<bool>(m));
+    b2[n - 1][m - 1] = true;
+    for (int i = n - 1; i >= 0; i--) {
+        for (int j = m - 1; j >= 0; j--) {
+            if (a[i][j] == '#')
+                continue;
+            if (i + 1 < n && b2[i + 1][j])
+                b2[i][j] = true;
+            if (j + 1 < m && b2[i][j + 1])
+                b2[i][j] = true;
+        }
+    }
+
+    int ans = INT_MAX;
+    for (int d = 1; d < (n + m - 2); d++) {
+        int cnt = 0;
+        for (int i = max(d - (m - 1), 0); i <= min(d, n - 1); i++) {
+            int j = d - i;
+            cnt += (b1[i][j] && b2[i][j]);
+        }
+        ans = min(ans, cnt);
+    }
+
+    cout << ans << endl;
+}
