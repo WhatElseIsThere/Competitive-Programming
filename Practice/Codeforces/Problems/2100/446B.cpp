45650e5a50cb36b01030614b18bc8cf5bd38b3c6
Artem Plotkin <arxmath@gmail.com>
2020-02-01 00:34:13 +0300

    Add problem 446B from CF

---
+++ Practice/Codeforces/Problems/2100/446B.cpp
@@ -0,0 +1,73 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fi first
+#define se second
+
+using ll = long long;
+using ld = long double;
+
+#define sz(x) int((x).size())
+#define all(x) begin(x), end(x)
+
+#ifdef LOCAL
+    #define eprint(x) cerr << #x << " = " << (x) << endl
+    #define eprintf(args...) fprintf(stderr, args), fflush(stderr)
+#else
+    #define eprint(x)
+    #define eprintf(...)
+#endif
+
+const int MAXN = 1000;
+
+int a[MAXN][MAXN];
+
+vector<ll> calc(const vector<int>& vals, int k, int dec) {
+    vector<ll> res = {0};
+    priority_queue<ll> q(all(vals));
+    for (int i = 1; i <= k; i++) {
+        ll val = q.top();
+        q.pop();
+        res.push_back(res.back() + val);
+        q.push(val - dec);
+    }
+    return res;
+}
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int n, m, k, p;
+    cin >> n >> m >> k >> p;
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            cin >> a[i][j];
+        }
+    }
+
+    vector<int> row_vals(n);
+    for (int i = 0; i < n; i++) {
+        for (int j = 0; j < m; j++) {
+            row_vals[i] += a[i][j];
+        }
+    }
+
+    vector<int> col_vals(m);
+    for (int j = 0; j < m; j++) {
+        for (int i = 0; i < n; i++) {
+            col_vals[j] += a[i][j];
+        }
+    }
+
+    auto r = calc(row_vals, k, m * p);
+    auto c = calc(col_vals, k, n * p);
+
+    ll ans = LLONG_MIN;
+    for (int i = 0; i <= k; i++) {
+        ans = max(ans, r[i] + c[k - i] - (i * 1LL * (k - i) * 1LL * p));
+    }
+
+    cout << ans << endl;
+}
